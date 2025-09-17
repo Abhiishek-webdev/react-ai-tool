d93ec0f6a7ecb7149216c0cae32f1662ca60ec2b
Abhiishek-webdev <abhishekp59022@gmail.com>
2025-09-17 17:44:28 +0530

    Update constants.js
    Remove Key

--- src/constants.js
+++ src/constants.js
@@ -1,1 +1,1 @@
-export const URL="https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent?key=AIzaSyALsjKEK33Jtj6parRJC5vlJKXsWMWPFao"
+export const URL="https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent?key="
