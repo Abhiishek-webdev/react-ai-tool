05798794c8c545e4d72932ed9876de70320bac12
Abhiishek-webdev <abhishekp59022@gmail.com>
2025-09-28 17:19:32 +0530

    initial-changes

--- src/constants.js
+++ src/constants.js
@@ -1,1 +1,1 @@
-export const URL="https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent?key="
+export const URL="https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent?key=AIzaSyAmk9uozH5PU3FqDDpH9GScMkiDWx_uvgU"
