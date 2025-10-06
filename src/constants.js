3ce0ec18d3a8cd81b47419397d1ee87d0b421060
Abhiishek-webdev <abhishekp59022@gmail.com>
2025-10-06 10:58:25 +0530

    fix api key

--- src/constants.js
+++ src/constants.js
@@ -1,1 +1,1 @@
-export const URL="https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent?key=AIzaSyAmk9uozH5PU3FqDDpH9GScMkiDWx_uvgU"
+export const URL="https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent?key=AIzaSyA6PRwtsZKSmJjbXqb7T_ItJDM8xdD9m7w"
